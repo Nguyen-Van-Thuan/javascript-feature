f282cb750f1f854667dea98d55d27555dec1fc88
thuan.nv <nguyenthuankma@gmail.com>
2022-10-05 16:15:21 +0700

    create data js

---
+++ Render-Array-Vanilla-javascript/data.js
@@ -0,0 +1,6 @@
+const userDetail = [
+  { name: "sunil", age: "24", place: "delhi", avatar: "./image/abc.jpg", country: "India" },
+  { name: "sujan", age: "22", place: "assam,", avatar: "./image/abc.jpg", country: "India" },
+  { name: "abishek", age: "26", place: "kolkata", avatar: "./image/abc.jpg", country: "India" },
+  { name: "chiranjeev", age: "20", place: "bangalore", avatar: "./image/abc.jpg", country: "India" },
+]
